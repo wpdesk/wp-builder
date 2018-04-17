cb587803853954ce66ecea020856ef6394cc5cdd
dyszczo <krzysztof@dyszczyk.pl>
2018-04-17 15:21:12 +0200

    requirements & hook fixes

--- src/Builder/AbstractBuilder.php
+++ src/Builder/AbstractBuilder.php
@@ -4,23 +4,18 @@ namespace WPDesk\PluginBuilder\Builder;
 
 use WPDesk\PluginBuilder\Plugin\AbstractPlugin;
 
-class AbstractBuilder
-{
-    const FILTER_PLUGIN_CLASS = 'wpdesk_plugin_class';
-	const HOOK_BEFORE_PLUGIN_INIT = 'wpdesk_before_plugin_init';
-	const HOOK_AFTER_PLUGIN_INIT = 'wpdesk_before_afterinit';
-
+class AbstractBuilder {
 	protected static $instances = [];
 
 	/**
 	 * @param string $class
 	 * @param AbstractPlugin $object
 	 */
-	protected function addToStorage($class, $object) {
-		if (isset(self::$instances[$class])) {
-			throw new Exception\ClassAlreadyExists("Class {$class} already exists");
+	protected function addToStorage( $class, $object ) {
+		if ( isset( self::$instances[ $class ] ) ) {
+			throw new Exception\ClassAlreadyExists( "Class {$class} already exists" );
 		}
-		self::$instances[$class] = $object;
+		self::$instances[ $class ] = $object;
 	}
 
 	/**
@@ -28,11 +23,11 @@ class AbstractBuilder
 	 *
 	 * @return AbstractPlugin
 	 */
-	protected function getFromStorage($class) {
-		if (isset(self::$instances[$class])) {
-			return self::$instances[$class];
+	protected function getFromStorage( $class ) {
+		if ( isset( self::$instances[ $class ] ) ) {
+			return self::$instances[ $class ];
 		} else {
-			throw new Exception\ClassNotExists("Class {$class} not exists in storage");
+			throw new Exception\ClassNotExists( "Class {$class} not exists in storage" );
 		}
 	}
 }
